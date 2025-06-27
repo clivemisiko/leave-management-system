b3db49ce26662bad37864cb307fc2b89c0bfa207
Clive Misiko <pappysjeux@gmail.com>
2025-06-27 13:05:08 +0300

    Updated config, dependencies, and .env loading

--- run.py
+++ run.py
@@ -1,11 +1,23 @@
 from dotenv import load_dotenv
 load_dotenv()
+
+from flask import redirect, url_for, session
 from backend.app import create_app
 
 app = create_app()
 
+@app.route('/')
+def index():
+    if session.get('admin_logged_in'):
+        return redirect(url_for('admin.admin_dashboard'))
+    elif session.get('staff_logged_in'):
+        return redirect(url_for('staff.staff_dashboard'))
+    else:
+        return redirect(url_for('staff.staff_login'))  # default fallback
+
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
 
-for rule in app.url_map.iter_rules():
-    print(f"{rule} --> {rule.endpoint}")
+    # Optional: print route map
+    for rule in app.url_map.iter_rules():
+        print(f"{rule} --> {rule.endpoint}")
